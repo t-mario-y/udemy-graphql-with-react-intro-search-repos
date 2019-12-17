dc406ee7e908cf2c227c67cc985c760d95e7c382
t-mario-y <t.mario.yoshi@gmail.com>
2019-12-17 17:38:02 +0900

    入力値をqueryに反映

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
 import React, { useState } from "react";
 import { ApolloProvider, Query } from "react-apollo";
-//import { useQuery } from "@apollo/react-hooks";
 import client from "./client";
 import SEARCH_RESPOSITORIES from "./graphql";
 
-const QUERY_VARIABLES = {
-  first: 10,
-  after: null,
-  last: null,
-  before: null,
-  query: "musescore"
-};
+//propsリレー失敗したのでコンポーネント分割を断念
+// const SearchStringForm = props => {
+//   //初期値の登録
+//   const [searchString, setSearchString] = useState(props.props.query);
+//   //入力値の反映
+//   const handleInputChange = e => {
+//     setSearchString(e.target.value);
+//     props.props.query = e.target.value;
+//     console.log(searchString);
+//   };
+//   //検索実行
+//   const handleSummit = e => {
+//     e.preventDefault();
+//   };
+//   return (
+//     <form onSubmit={handleSummit}>
+//       >
+//       <input type="text" value={searchString} onChange={handleInputChange} />
+//     </form>
+//   );
+// };
 
 const App = () => {
+  const QUERY_VARIABLES = {
+    first: 10,
+    after: null,
+    last: null,
+    before: null,
+    query: "musescore"
+  };
+  //初期値の登録
+  const [searchString, setSearchString] = useState(QUERY_VARIABLES.query);
+  //入力値をqueryに反映
+  const handleInputChange = e => {
+    setSearchString(e.target.value);
+    QUERY_VARIABLES.query = e.target.value;
+    console.log("handleInputChange called");
+  };
+
+  //検索実行
+  const handleSubmit = e => {
+    console.log("handleSummit called");
+    e.preventDefault();
+  };
   return (
     <ApolloProvider client={client}>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={searchString} onChange={handleInputChange} />
+      </form>
       <Query query={SEARCH_RESPOSITORIES} variables={QUERY_VARIABLES}>
         {({ loading, error, data }) => {
           if (loading) return "Loading....";
