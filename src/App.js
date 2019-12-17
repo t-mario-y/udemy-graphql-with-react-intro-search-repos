5d1755c14202870c38cc3c3545e62f0cf5ff7688
t-mario-y <t.mario.yoshi@gmail.com>
2019-12-17 17:46:36 +0900

    タイトルの表記修正

--- src/App.js
+++ src/App.js
@@ -3,28 +3,6 @@ import { ApolloProvider, Query } from "react-apollo";
 import client from "./client";
 import SEARCH_RESPOSITORIES from "./graphql";
 
-//propsリレー失敗したのでコンポーネント分割を断念
-// const SearchStringForm = props => {
-//   //初期値の登録
-//   const [searchString, setSearchString] = useState(props.props.query);
-//   //入力値の反映
-//   const handleInputChange = e => {
-//     setSearchString(e.target.value);
-//     props.props.query = e.target.value;
-//     console.log(searchString);
-//   };
-//   //検索実行
-//   const handleSummit = e => {
-//     e.preventDefault();
-//   };
-//   return (
-//     <form onSubmit={handleSummit}>
-//       >
-//       <input type="text" value={searchString} onChange={handleInputChange} />
-//     </form>
-//   );
-// };
-
 const App = () => {
   const QUERY_VARIABLES = {
     first: 10,
@@ -56,10 +34,16 @@ const App = () => {
         {({ loading, error, data }) => {
           if (loading) return "Loading....";
           if (error) return `Error! ${error.message}`;
+          const search = data.search;
+          const repositoryCount = search.repositoryCount;
+          const repositoryUnit =
+            repositoryCount === 1 ? "Repository" : "Repositories";
+          const title = `GitHub Repositories Search Result: ${repositoryCount} ${repositoryUnit}`;
           return (
             <React.Fragment>
+              <h1>{title}</h1>
               <ul>
-                {data.search.edges.map(edge => {
+                {search.edges.map(edge => {
                   const node = edge.node;
                   return (
                     <li key={node.id}>
