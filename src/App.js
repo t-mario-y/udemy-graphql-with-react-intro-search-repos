613b2ef4d2fec4b18fefc2d10d1e105dfe936d84
t-mario-y <t.mario.yoshi@gmail.com>
2019-12-17 15:23:41 +0900

    queryにvariableを埋め込み

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { ApolloProvider, Query } from "react-apollo";
+//import { useQuery } from "@apollo/react-hooks";
 import client from "./client";
-import ME from "./graphql";
+import SEARCH_RESPOSITORIES from "./graphql";
+
+const QUERY_VARIABLES = {
+  first: 10,
+  after: null,
+  last: null,
+  before: null,
+  query: "musescore"
+};
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Hello GraphQL.</div>
-      <Query query={ME}>
+      <Query query={SEARCH_RESPOSITORIES} variables={QUERY_VARIABLES}>
         {({ loading, error, data }) => {
           if (loading) return "Loading....";
           if (error) return `Error! ${error.message}`;
-          return <div>{data.user.name}</div>;
+          return (
+            <React.Fragment>
+              <ul>
+                {data.search.edges.map(edge => {
+                  const node = edge.node;
+                  return (
+                    <li>
+                      <a href={node.url} target="#">
+                        {node.nameWithOwner}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </React.Fragment>
+          );
         }}
       </Query>
     </ApolloProvider>
--- src/graphql.js
+++ src/graphql.js
@@ -1,12 +1,53 @@
 import gql from "graphql-tag";
 
-const ME = gql`
-  query me {
-    user(login: "t-mario-y") {
-      name
-      avatarUrl
+const SEARCH_RESPOSITORIES = gql`
+  query searchRepositoriesWithPagination(
+    $first: Int
+    $after: String
+    $last: Int
+    $before: String
+    $query: String!
+  ) {
+    search(
+      first: $first
+      after: $after
+      last: $last
+      before: $before
+      query: $query
+      type: REPOSITORY
+    ) {
+      repositoryCount
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            nameWithOwner
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
     }
   }
 `;
 
-export default ME;
+export default SEARCH_RESPOSITORIES;
+
+// export const ME = gql`
+//   query me {
+//     user(login: "t-mario-y") {
+//       name
+//       avatarUrl
+//     }
+//   }
+// `;
